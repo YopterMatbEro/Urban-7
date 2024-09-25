222758191c0da2bd9974bf0174b5b88319431598
YopterMatbEro <yopter2013@gmail.com>
2024-09-25 13:35:49 +0300

    files in OS

--- module_7_files_in_OS.py
+++ module_7_files_in_OS.py
@@ -13,6 +13,5 @@ for root, dirs, files in os.walk(directory):
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
         filesize = os.path.getsize(filepath)
         parent_dir = os.path.dirname(filepath)
-        print(
-            f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, \
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, \
 Родительская директория: {parent_dir}')
